e7ee3f9bceff889ae5c0365e19b5cc974daeb45d
chris <turabayochryso98@gmail.com>
2025-04-28 12:21:39 +0900

    added env var6

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,32 @@
 import Link from 'next/link'
-import { BASE_API_URL } from "./utils/constants"
+// import { BASE_API_URL } from "./utils/constants"
 
 export default function Home() {
-  if(!BASE_API_URL) {
-    return null
-  }
-  return (
-    <main>
-      <h2>Dashboard</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
+  return(<>test</>)
+  // if(!BASE_API_URL) {
+  //   return null
+  // }
+  // return (
+  //   <main>
+  //     <h2>Dashboard</h2>
+  //     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
 
-      <div className="flex justify-center my-8">
-        <Link href="/tickets">
-          <button className="btn-primary">View Tickets</button>
-        </Link>
-      </div>
+  //     <div className="flex justify-center my-8">
+  //       <Link href="/tickets">
+  //         <button className="btn-primary">View Tickets</button>
+  //       </Link>
+  //     </div>
 
-      <h2>Company Updates</h2>
+  //     <h2>Company Updates</h2>
 
-      <div className="card">
-        <h3>Expanding flights...</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
-      </div>
-      <div className="card">
-        <h3>Recruiting crew members</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
-      </div>
-    </main>
-  )
+  //     <div className="card">
+  //       <h3>Expanding flights...</h3>
+  //       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
+  //     </div>
+  //     <div className="card">
+  //       <h3>Recruiting crew members</h3>
+  //       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
+  //     </div>
+  //   </main>
+  // )
 }
